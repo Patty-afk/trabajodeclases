f85141fdabadbe436a7a8d4b6855cdf9806a9d5d
Patty <l22121084@morelia.tecnm.mx>
2024-09-09 22:21:39 -0600

    primer ejemplo de python

---
+++ main.py
@@ -0,0 +1,17 @@
+from persona import Persona
+
+#Instancia de la clase persona
+persona_uno = Persona("Maria", 20, 1.65, 60)
+persona_uno.mostrar_info()
+
+#Segunda instancia de la clase persona
+print("****************")
+persona_dos = Persona("Alberto", 22, 1.89, 90.5)
+persona_dos.mostrar_info()
+
+
+#Instancia de la clase persona
+print("****************")
+persona_tres = Persona("Edson", 10, 1.91, 85.5)
+persona_tres.mostrar_info()
+
